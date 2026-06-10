8f16aaa4262f3987c4cb44259d7612210161db25
Dhanraj166 <dhanrajs781004@gmail.com>
2026-06-10 21:31:18 +0530

    Divisibility of 11 pratice

--- divisibility.py
+++ divisibility.py
@@ -2,10 +2,7 @@
 
 n = int(input("Enter a number : "))
 digit = n%10
-if digit%5 == 0:
-    print(True)
-else:
-    print(False)
+print(digit%5 == 0)
 
 
 
@@ -13,15 +10,12 @@ else:
 
 n = int(input("Enter a number : "))
 digit = n%100
-if digit%4 == 0:
-    print(True)
-else:
-    print(False)
+print(digit%4 == 0)
 
 
 # Divisibility by 6
 
-no = 12345
+no = int(input("Enter a number : "))
 total = 0
 digit = no%10
 while no>0:
@@ -45,13 +39,53 @@ while(n>0):
     rem = n%10
     totel += rem
     n = n // 10
-if(totel%9==0):
-    print('Divisible by 9')
-else:
-    print('Not Divisible by 9')
+print(totel%9==0)
+# if(totel%9==0):
+#     print('Divisible by 9')
+# else:
+#     print('Not Divisible by 9')
 
 
 # Divisibility by 10
 
 n = int(input("Enter a number : "))
 print(n%10 == 0)
+
+
+# Divisibility by 11
+
+n = int(input("Enter a number : "))
+temp = n//10
+odd = 0
+even = 0
+while(n>0):
+    rem = n%10
+    odd = odd + rem
+    n = n // 100
+
+while(temp>0):
+    rem = temp%10
+    even = even + rem
+    temp = temp // 100
+    
+if((odd-even)%11 == 0):
+    print('divisibility of 11')
+else:
+    print('Not divisibility of 11')
+
+
+
+# Divisibility by 11
+
+n = int(input("Enter a number : "))
+
+odd = 0
+even = 0
+
+while(n>0):
+    rem = n%100
+    odd = odd + rem % 10
+    even = even + rem // 10
+    n = n // 100
+print((odd-even)%11 == 0)
+
